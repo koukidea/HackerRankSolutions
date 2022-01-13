cd314681a716fa6574a63fb63a457bfe4f8ef5c9
koukidea <ktkmert0@gmail.com>
2022-01-13 10:15:20 +0300

    Sum and Difference of Two Numbers

---
+++ Solution_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x,y;
+    float z,t;
+    
+    scanf("%d %d", &x, &y);
+    
+    scanf("%f %f", &z, &t);
+    
+    
+    printf("%d %d\n", x+y, x-y);
+    printf("%.1f %.1f", z+t, z-t);
+    
+    return 0;
+}
